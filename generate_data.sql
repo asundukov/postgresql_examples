f9a086f872d75f472a1c851da6f53ec3646730a3
Сундуков Андрей <asundukov@users.noreply.github.com>
2017-02-25 07:19:55 +0300

    Update generate_data.sql

--- generate_data.sql
+++ generate_data.sql
@@ -33,11 +33,11 @@ SELECT p.person_id, concat(md5(cast(random() as text))) as title, concat('http:/
 FROM (SELECT p.person_id FROM person p WHERE 1 = round(random()*3)) p
 LEFT JOIN generate_series(1,10) AS t(num) ON (true);
 
-/* or add random count (from 1 to k) photos for each person (k=10) */
-INSERT INTO photo (person_id, title, url)
+/* multiply current photo count for active users k times (k = 1.7) */
+postgres=# INSERT INTO photo (person_id, title, url)
 SELECT p.person_id, concat(md5(cast(random() as text))) as title, concat('http://', md5(cast(random() as text)), '.ru') as url
-FROM person p
-LEFT JOIN generate_series(1,10) AS t(num) ON (t.num < (random() * 10));
+FROM photo p
+WHERE 7 > round(random()*10);
 
 /* see result allocation */
 SELECT 
